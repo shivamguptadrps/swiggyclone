f49100bc8d63eb931d56e781aea07f1a9c3844a3
Shivam Gupta <shivamguptadrps210001@gmail.com>
2023-09-17 13:45:05 +0530

    Updates

---
+++ Grocery.cdfab9f1.js
@@ -0,0 +1,3 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire164e;e.register("bdzuP",function(n,o){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var i=e("jPxf6"),l=()=>/*#__PURE__*/(0,i.jsx)("h1",{children:"this is large companent and has n number of component"})});//# sourceMappingURL=Grocery.cdfab9f1.js.map
+
+//# sourceMappingURL=Grocery.cdfab9f1.js.map
